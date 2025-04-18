01b6852863603f2e11cc8e7ff71883a7bfc0f80e
siting <sesiting@163.com>
2025-04-18 18:35:28 +0800

    初始化项目并提交到dev分支

---
+++ src/main/java/com/example/toolweb/ToolWebApplication.java
@@ -0,0 +1,15 @@
+package com.example.toolweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 应用程序入口
+ */
+@SpringBootApplication
+public class ToolWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolWebApplication.class, args);
+    }
+} 
